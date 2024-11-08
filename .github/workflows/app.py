e8c8cd9a5347fd93a6520bcb42b407ac2dbd9674
saipriya <saimuppidi2003@gmail.com>
2024-11-08 11:07:06 +0530

    file change

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,20 +1,17 @@
-from flask import Flask, redirect, url_for
-
-app = Flask(__name__)
+from flask import Flask
+app=Flask(__name__)
 
 @app.route("/")
 def home():
-    # Redirect to the 'about' route instead of returning text
-    return redirect(url_for("https://devopsaws-gitactions.s3.us-east-1.amazonaws.com/netflixstyles.css"))
+    return "https://devopsaws-gitactions.s3.us-east-1.amazonaws.com/index.html"
 
-@app.route("/netflixstyles")
-def netflixstyles():
-    return "<h2>Welcome to our About Page....</h2>"
+@app.route("/about")
+def about():
+    return "https://devopsaws-gitactions.s3.us-east-1.amazonaws.com/netflixstyles.css"
 
 @app.route("/service")
 def service():
-    # Redirect to an external URL or another internal route if needed
-    return redirect("https://devopsaws-gitactions.s3.us-east-1.amazonaws.com/netflixstyles.css")  # For an external site
+    return "<h1>Welcome to  our Service page.....</h1>"
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ =='__main__':
+    app.run(host="0.0.0.0",port=5000)
