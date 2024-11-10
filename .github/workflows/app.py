2fd56db8c693b0cddc50040b438ff5d7f2da83f0
saipriya <saimuppidi2003@gmail.com>
2024-11-10 19:50:17 +0530

    file change1

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("index.html")
 
 @app.route("/service")
 def service():
-    return render_template("service.html")
+    return render_template("netflixstyles.css")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
